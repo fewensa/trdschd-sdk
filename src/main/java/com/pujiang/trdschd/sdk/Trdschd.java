6170b31ed3f9cf5e8d474c5342357854146b69a3
fewensa <fewensa@protonmail.com>
2020-10-10 10:48:16 +0800

    fill

--- src/main/java/com/pujiang/trdschd/sdk/Trdschd.java
+++ src/main/java/com/pujiang/trdschd/sdk/Trdschd.java
@@ -1,18 +1,33 @@
 package com.pujiang.trdschd.sdk;
 
+import com.pujiang.trdschd.sdk.consumer.TrdschdConsumer;
 import com.pujiang.trdschd.sdk.producer.TrdschdProducer;
 
 public class Trdschd {
 
 
-  private Config config;
+  private final Config config;
 
+  public Trdschd(Config config) {
+    this.config = config;
+  }
+
+  public static Trdschd host(String host) {
+    Config config = Config.builder()
+      .host(host)
+      .build();
+    return new Trdschd(config);
+  }
 
   public TrdschdProducer producer(String producerId) {
     this.config.setProducerId(producerId);
     return new TrdschdProducer(this.config);
   }
 
+  public TrdschdConsumer consumer(String consumerId) {
+    this.config.setConsumerId(consumerId);
+    return new TrdschdConsumer(this.config);
+  }
 
 
 }
